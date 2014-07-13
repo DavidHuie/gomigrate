5cd2e24d988fed0f50f623900864bdd0b1eb132d
David Huie <dahuie@gmail.com>
2014-07-13 12:39:48 -0700

    More refactoring

--- utils.go
+++ utils.go
@@ -15,31 +15,27 @@ var (
 func parseMigrationPath(path string) (uint64, migrationType, string, error) {
 	filebase := filepath.Base(path)
 
-	// Check to see if this is a up migration.
 	matches := upMigrationFile.FindAllSubmatch([]byte(filebase), -1)
 	if matches != nil {
-		num := matches[0][1]
-		name := matches[0][2]
-		parsedNum, err := strconv.ParseUint(string(num), 10, 64)
-		if err != nil {
-			return 0, "", "", err
-		}
-		return parsedNum, upMigration, string(name), nil
+		return parseMatches(matches, upMigration)
 	}
-
-	// Down migration.
 	matches = downMigrationFile.FindAllSubmatch([]byte(filebase), -1)
 	if matches != nil {
-		num := matches[0][1]
-		name := matches[0][2]
-		parsedNum, err := strconv.ParseUint(string(num), 10, 64)
-		if err != nil {
-			return 0, "", "", err
-		}
-		return parsedNum, downMigration, string(name), nil
+		return parseMatches(matches, downMigration)
 	}
 
 	return 0, "", "", InvalidMigrationFile
+}
+
+// Parses matches given by a migration file regex.
+func parseMatches(matches [][][]byte, mType migrationType) (uint64, migrationType, string, error) {
+	num := matches[0][1]
+	name := matches[0][2]
+	parsedNum, err := strconv.ParseUint(string(num), 10, 64)
+	if err != nil {
+		return 0, "", "", err
+	}
+	return parsedNum, mType, string(name), nil
 }
 
 // This type is used to sort migration ids.
